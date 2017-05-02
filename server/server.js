066eb3d17af94026678d6ce4aca9c87f99687509
pashkoostap <pashko.ostap@gmail.com>
2017-05-02 20:10:04 +0300

    added init page for project

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const bodyParser = require('body-parser');
 
 const router = require('./routes');
+const publicPath = path.join(__dirname, '../public');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +14,8 @@ const io = socketIO(server);
 
 app.use(bodyParser.json());
 app.use(router);
+
+app.use(express.static(publicPath));
 
 // io.on('connection', (socket) => {
 //   console.log(socket);
